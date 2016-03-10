5694e5ddb02f9dd0210412445c27cb9218c8f26a
Chiho <gm@haeyum.org>
2016-03-10 11:13:06 +0900

    refactored

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -18,12 +18,14 @@ import org.slf4j.LoggerFactory;
 import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
+import util.HttpRequestUtils.Pair;
 import util.IOUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
 	private Socket connection;
+	private Map<String,String> headerContents = new HashMap<String,String>();
 
 	public RequestHandler(Socket connectionSocket) {
 		this.connection = connectionSocket;
@@ -34,115 +36,139 @@ public class RequestHandler extends Thread {
 				connection.getPort());
 
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			
-			// 버퍼리더를 이용해 url 추출 
+
+			// 버퍼리더를 이용해 리퀘스트 헤더 추출 
 			BufferedReader bfr = new BufferedReader(new InputStreamReader(in));
-			String line = bfr.readLine();
-			String[] tokens = line.split(" ");
-			String url = tokens[1];
-			int index = url.indexOf("?");
-			String requestPath = url;
-			String params = "";
+
+			// url 추출 
+			getUrl(bfr.readLine());
+
+			parseHeader(bfr);
 			
-			// 파라미터가 있을 경우 
-			if (index != -1) {
-				requestPath = url.substring(0, index);
-				params = url.substring(index + 1);
-			}
-
-			int contentLength = 0;
-			Map<String, String> cookie = new HashMap<String, String>();
-			String type = "";
-			while (!"".equals(line)) {
-				
-				if (line == null) {
-					break;
-				}
-				System.out.println(line);
-				line = bfr.readLine();
-				String[] token = line.split(" ");
-				if (token[0].equals("Content-Length:")) {
-					contentLength = Integer.parseInt(token[1]);
-				}else if(token[0].equals("Cookie:")){
-					cookie = HttpRequestUtils.parseCookies(token[1]);
-				}else if(token[0].equals("Accept:")){
-					String[] acceptToken = token[1].split(",");
-					type = acceptToken[0];
-				}
-					
-				
-			}
-			System.out.println("\n");
-			String content = IOUtils.readData(bfr, contentLength);
-
-			DataOutputStream dos = new DataOutputStream(out);
-
-			// 요청 url별 처리 
-			if (requestPath.equals("/") || requestPath.equals("/") ) {
-				byte[] body = Files.readAllBytes(new File("./webapp/index.html").toPath());
-				response200Header(dos, body.length, "", "text/html");
-				responseBody(dos, body);
-			} 
-			else if (requestPath.equals("/user/list.html")){
-				if(cookie.get("logined").equals("true")){
-					String html = new String(Files.readAllBytes(new File("./webapp/user/list.html").toPath()));
-					StringBuilder sb = new StringBuilder(html);
-					String userListHtml = "";
-					int userNum = 1;
-					for(User user : DataBase.findAll()){
-						userListHtml+="<tr>";
-						userListHtml+="<th scope=\"row\">";
-						userListHtml+=userNum;
-						userListHtml+="</th>";
-						userListHtml+="<td>";
-						userListHtml+=user.getUserId();
-						userListHtml+="</td>";
-						userListHtml+="<td>";
-						userListHtml+=user.getName();
-						userListHtml+="</td>";
-						userListHtml+="<td>";
-						userListHtml+=user.getEmail();
-						userListHtml+="</td>";
-						userListHtml+="<td>";
-						userListHtml+="<a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a>";
-						userListHtml+="</td>";
-						userListHtml+="</tr>";
-						++userNum;						
-					}
-					sb.replace(sb.indexOf("<%userlist%>"), sb.indexOf("<%userlist%>")+"<%userlist%>".length(), userListHtml);
-					byte[] body = sb.toString().getBytes();
-					
-					response200Header(dos, body.length,"", "text/html");
-					responseBody(dos, body);
-				}else{
-					response302Header(dos, "/user/login.html","");
-				}
-			}
-			else if (requestPath.equals("/user/create")) {
-				addMember(content);
-				response302Header(dos, "/index.html","");
-			} 
-			else if(requestPath.equals("/user/login")){
-				Map<String, String> loginParams = HttpRequestUtils.parseQueryString(content);
-				User user = DataBase.findUserById(loginParams.get("userId"));
-				System.out.println(loginParams.get("password") + " " + user.getPassword());
-				if(user != null && user.getPassword().equals(loginParams.get("password"))){
-					response302Header(dos, "/index.html","logined=true;");
-				}
-				else{
-					response302Header(dos, "/index.html","logined=false;");
-				}
-			}
-			else {
-				byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
-				response200Header(dos, body.length,"", type);
-				responseBody(dos, body);
-			}
+			response(out);
+			
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
 	}
+		
+	private void response(OutputStream out) throws IOException {
+		//response 처
+		Map<String, String> cookie = HttpRequestUtils.parseCookies(headerContents.get("Cookie"));
+		String requestPath = headerContents.get("Request-Path");
+		DataOutputStream dos = new DataOutputStream(out);
+		String type = headerContents.get("Accept").split(",")[0];
+			
+		// 요청 url별 처리 
+		if (requestPath.equals("/") || requestPath.equals("") ) {
+			
+			byte[] body = Files.readAllBytes(new File("./webapp/index.html").toPath());
+			response200Header(dos, body.length, "", "text/html");
+			responseBody(dos, body);
+		} 
+		//user/list
+		else if (requestPath.equals("/user/list.html")){
+			if(cookie.get("logined").equals("true")){
+				//User/List.html 생성해서 전
+				String html = new String(Files.readAllBytes(new File("./webapp/user/list.html").toPath()));
+				StringBuilder sb = new StringBuilder(html);
+//				
+				String userListHtml = "";
+				String format = "<tr>"
+						+ "<th>%d</th>"
+						+ "<td>%s</td>"
+						+ "<td>%s</td>"
+						+ "<td>%s</td>"
+						+ "<td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>"
+						+ "</tr>";
 
+				int userNum = 1;
+				for(User user : DataBase.findAll()){
+					userListHtml += String.format(format, userNum, user.getUserId(), user.getName(), user.getEmail());
+				}
+				
+				sb.replace(sb.indexOf("<%userlist%>"), sb.indexOf("<%userlist%>")+"<%userlist%>".length(), userListHtml);
+				byte[] body = sb.toString().getBytes();
+				
+				response200Header(dos, body.length,"", "text/html");
+				responseBody(dos, body);
+			}else{
+				response302Header(dos, "/user/login.html","");
+			}
+		}
+		//새로운 멤버 추가
+		else if (requestPath.equals("/user/create")) {
+			String content = headerContents.get("Content");
+			addMember(content);
+			response302Header(dos, "/index.html","");
+		} 
+		//로그인
+		else if(requestPath.equals("/user/login")){
+
+			String content = headerContents.get("Content");
+			Map<String, String> loginParams = HttpRequestUtils.parseQueryString(content);
+			User user = DataBase.findUserById(loginParams.get("userId"));
+			
+			if(user != null && user.getPassword().equals(loginParams.get("password"))){
+				response302Header(dos, "/index.html","logined=true;");
+			}
+			else{
+				response302Header(dos, "/user/login_failed.html","logined=false;");
+			}
+		}
+		//페이지 파일 읽어서 보내
+		else {
+			byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
+			response200Header(dos, body.length,"", type);
+			responseBody(dos, body);
+		}
+	}
+
+	private void parseHeader(BufferedReader bfr) throws IOException {
+		String line = bfr.readLine();
+		
+		//header variable parsing
+		while (!"".equals(line)) {
+			
+			if (line == null) {
+				break;
+			}
+			System.out.println(line);
+			Pair headerContent = HttpRequestUtils.parseHeader(line);
+			headerContents.put(headerContent.getKey(), headerContent.getValue());
+
+			line = bfr.readLine();
+		}
+		System.out.println("");
+		
+		if(headerContents.get("Method").equals("POST")){
+			int contentLength = Integer.parseInt(headerContents.get("Content-Length"));
+			String content = IOUtils.readData(bfr, contentLength);
+			headerContents.put("Content",content);
+		}
+		
+	}
+
+	private void getUrl(String line) {
+		String[] tokens = line.split(" ");
+		String url = tokens[1];
+		int index = url.indexOf("?");
+		
+		String requestPath = url;
+		String params = "";
+
+		// 파라미터가 있을 경우 
+		if (index != -1) {
+			requestPath = url.substring(0, index);
+			params = url.substring(index + 1);
+		}
+
+		headerContents.put("Method", tokens[0]);
+		headerContents.put("Request-Path", requestPath);
+		headerContents.put("Params", params);
+	}
+
+//response
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String cookieQuery, String type) {
 		try {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
